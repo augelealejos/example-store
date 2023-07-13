47032f29472c6bfb0ae07f43e2f5fe326b5e6b3d
Augele Alejos <augelealejos@gmail.com>
2023-07-13 18:08:37 -0400

    [ADD] pantallas de productos y categorias, funcionalidad de seleccionar categoria

---
+++ src/screens/categories/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+import { COLORS } from "../../themes";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.background,
+  },
+  categoryContainer: {
+    flex: 1,
+  },
+  listCategory: {
+    paddingBottom: 20,
+  },
+});
