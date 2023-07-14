12592388595cdd04a35b2e7b7a157d7031b4e846
Augele Alejos <augelealejos@gmail.com>
2023-07-13 23:47:40 -0400

    [FIX] run prettier other files

--- src/components/products/item/styles.js
+++ src/components/products/item/styles.js
@@ -7,14 +7,24 @@ export const styles = StyleSheet.create({
     gap: 15,
   },
   productContainer: {
-    backgroundColor: COLORS.primary,
+    backgroundColor: COLORS.white,
     borderRadius: 10,
     width: "45%",
     marginHorizontal: 10,
+    shadowColor: COLORS.black,
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
   },
   productImage: {
     width: "100%",
     height: 150,
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
   },
   productDetail: {
     padding: 10,
@@ -22,9 +32,10 @@ export const styles = StyleSheet.create({
   },
   productName: {
     fontSize: 14,
+    fontFamily: "Rubik-Light",
   },
   productPrice: {
     fontSize: 15,
-    fontWeight: "bold",
+    fontFamily: "Rubik-Light",
   },
-});
+}); 
