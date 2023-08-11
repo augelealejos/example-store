128e9e4540ea2cb4893aa67316985fdd5aab1db3
Augele Alejos <augelealejos@gmail.com>
2023-08-10 23:26:34 -0400

    [ADD] menu profile y address

--- src/constants/data/menu.js
+++ src/constants/data/menu.js
@@ -1,14 +1,14 @@
 export const MENUS = [
     {
-      id: '1',
-      icon: 'ios-person-sharp',
-      route: 'Profile',
-      title: 'Profile',
+      id: "1",
+      icon: "ios-person-sharp",
+      route: "Profile",
+      title: "Profile",
     },
     {
-      id: '2',
-      icon: 'ios-location-sharp',
-      route: 'Address',
-      title: 'Address',
+      id: "2",
+      icon: "ios-location-sharp",
+      route: "Address",
+      title: "Address",
     },
   ];
